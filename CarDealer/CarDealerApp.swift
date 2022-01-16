1b0de85a19191a98c2893f21a781c258dd37590a
mohak92 <tamhane.mohak@gmail.com>
2022-01-16 17:46:38 -0500

    Initial Commit

---
+++ CarDealer/CarDealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerApp.swift
+//  CarDealer
+//
+//  Created by Mohak Tamhane on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CarDealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarDealer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarDealer
+//
+//  Created by Mohak Tamhane on 1/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
