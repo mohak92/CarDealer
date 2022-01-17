254228c8d695847bdf43ff9921d552a509d904dd
mohak92 <tamhane.mohak@gmail.com>
2022-01-17 16:17:02 -0500

    Car Object

---
+++ CarDealer/Models/Car.swift
@@ -0,0 +1,17 @@
+//
+//  Car.swift
+//  CarDealer
+//
+//  Created by Mohak Tamhane on 1/17/22.
+//
+
+import Foundation
+
+struct Car {
+    
+    var id: Int
+    var carName: String
+    var carImage: String
+    var carPrice: String
+    
+}
